d3e697e0c0f80c5a3e5783011fcd10566d225d93
liyunpeng <liyunpeng@zerozero.cn>
2024-12-23 19:59:15 +0800

    diagnostic: modify diagnostic icon

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -2,6 +2,12 @@ local M = {}
 local map = vim.keymap.set
 local mason_lspconfig = require 'mason-lspconfig'
 
+local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "󰋼" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 local servers = {
     clangd = {},
     pyright = {},
