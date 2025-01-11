99cd5019e9e9ae25ca46d80c7ea512ecaa7d3d43
liyunpeng <liyunpeng@zerozero.cn>
2025-01-11 23:27:14 +0800

    keymap: set L and H function

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,8 @@ opt.cursorline = true
 
 
 -- 大小的L和H映射
-map({ "n", "t" }, "L", "End", { noremap = true, silent = true,  desc = "to end of line" })
-map({ "n", "t" }, "H", "<ESC>^i", { noremap = true, silent = true,  desc = "to begin of line" })
+map({ "n", "t" }, "L", "$", { noremap = true, silent = true,  desc = "to end of line" })
+map({ "n", "t" }, "H", "<ESC>^", { noremap = true, silent = true,  desc = "to begin of line" })
 map({ "n", "t" }, "dL", "d$", { noremap = true, silent = true, desc = "del to end of line" })
 map({ "n", "t" }, "dH", "d0", { noremap = true, silent = true, desc = "del to begin of line" })
 
