91da1fed550f132c4b32f713d03382de841a6656
liyunpeng <liyunpeng@zerozero.cn>
2025-07-03 23:21:13 +0800

    key: add q to esc when visual mode

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ vim.g.maplocalleader = ","
 map("n", " ", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "kj", "<ESC>")
+map("v", "q", "<Esc>")
 map("n", "<leader>q", "<cmd>q<CR>", { desc = "CMD quit" })
 map("n", "<leader>x", "<cmd>qa<CR>", { desc = "CMD quit all" })
 
