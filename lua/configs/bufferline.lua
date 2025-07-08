b5440e575d349e9b31e13d2f402e22b184583b3f
liyunpeng <liyunpeng@zerozero.cn>
2025-07-08 22:37:49 +0800

    bufferline: del num show in first for more space to file name

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -3,15 +3,15 @@ local M = {}
 
 M.options = {
     themable = false, -- allows highlight groups to be overriden i.e. sets highlights as default
-        numbers = "ordinal",
+    -- numbers = "ordinal",
     close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
-        left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
-        middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
-        indicator = {
-            icon = '▊',
-            style = 'icon',
-        },
+    right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
+    indicator = {
+        icon = '▊',
+        style = 'icon',
+    },
     offsets = {
         {
             filetype = "NvimTree",
