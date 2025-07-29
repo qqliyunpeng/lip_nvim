a3704c377a53bf12fe1fcbcd071c9b92555c7890
liyunpeng <liyunpeng.syq@aohua.com>
2025-07-29 13:34:41 +0800

    textobj: <a-;> to repeat_last_move_previous

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -79,7 +79,7 @@ M.textobjectsConfig = function()
     -- not work every times
     local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
     vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, "<C-;>", ts_repeat_move.repeat_last_move_previous)
+    vim.keymap.set({ "n", "x", "o" }, "<a-;>", ts_repeat_move.repeat_last_move_previous)
 end
 
 return M
