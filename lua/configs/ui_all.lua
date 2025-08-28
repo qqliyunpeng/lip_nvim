7dddba39b649e249af858622e0beba8727fb714a
liyunpeng <liyunpeng.syq@aohua.com>
2025-08-28 13:12:48 +0800

    conceallevel: disable it

--- lua/configs/ui_all.lua
+++ lua/configs/ui_all.lua
@@ -196,7 +196,6 @@ M.snacksInit = function ()
             Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
             Snacks.toggle.diagnostics():map("<leader>ud")
             Snacks.toggle.line_number():map("<leader>ul")
-            Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
             Snacks.toggle.inlay_hints():map("<leader>uh")
         end,
     })
