8139e3e09af2e938e863fecfd74648cdf00d4a79
liyunpeng <liyunpeng.syq@aohua.com>
2025-09-24 13:01:27 +0800

    perf: pref --append

--- lua/plugins/lip_plugins.lua
+++ lua/plugins/lip_plugins.lua
@@ -121,13 +121,10 @@ return {
     {
         "https://gitee.com/yunduozhai/nvim-treesitter-textobjects.git",
         event = { "BufReadPost", "BufNewFile" },
-    --     dependencies = {
-    --         "nvim-treesitter",
-    --     },
-    config = function()
-        return require("configs.treesitter").textobjectsConfig()
-    end
-     },
+        config = function()
+            return require("configs.treesitter").textobjectsConfig()
+        end
+    },
     {
         -- vaq/viq 选中单引号或者双引号等各种引号之间的内容
         "https://gitee.com/nvim_lip/vim-textobj-quotes.git",
