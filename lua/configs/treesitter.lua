bff88846c1506eb0c92b7ead9a320e3e73602c05
liyunpeng <liyunpeng@zerozero.cn>
2026-02-11 11:05:03 +0800

    treesitter: add commits in some err

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,4 +1,4 @@
-
+-- 如果经常报错，尝试: rm -rf ~/.local/share/nvim/site/parser
 local M = {}
 
 function M.textobjectsConfig()
@@ -96,7 +96,6 @@ function M.treesitterConfig()
                 goto_next_end   = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
                 goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
                 goto_previous_end   = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-
 
                 goto_next     = { ["]d"] = "@conditional.outer", },
                 goto_previous = { ["[d"] = "@conditional.outer", },
