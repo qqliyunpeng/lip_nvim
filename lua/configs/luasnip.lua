b090879b65dc2b6c8e7e2c2020767073dcf7ee14
liyunpeng <liyunpeng.syq@aohua.com>
2026-05-13 11:26:38 +0800

    fix: fix warning in luasnip for inc <tab>.

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -1,3 +1,22 @@
+-- Compatibility for legacy snipmate snippets using `Filename()` in backticks.
+-- LuaSnip evaluates those snippets through Vimscript, so this must be a real
+-- Vimscript function rather than a Lua callback stored in vim.fn.
+vim.cmd([[
+function! Filename(...) abort
+    let l:name = expand('%:t:r')
+
+    if l:name ==# ''
+        return a:0 >= 2 ? a:2 : ''
+    endif
+
+    if a:0 >= 1 && a:1 !=# ''
+        return substitute(a:1, '\$1', l:name, 'g')
+    endif
+
+    return l:name
+endfunction
+]])
+
 -- snipmate format
 require("luasnip.loaders.from_snipmate").load()
 require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
